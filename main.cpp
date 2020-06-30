e03793f22e5018928ac2c5af77f9eaf8e1bab7f0
saulg7746 <45016926+saulg7746@users.noreply.github.com>
2020-06-30 12:04:49 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,11 +3,11 @@
 int main()
 {
 	Solutions solutions;
-	std::vector < int > nums = { 1,3,-1,-3,5,3,6,7 };
-	int k = 3;
-	std::vector<double> ans = solutions.medianSlidingWindow(nums, k);
-	for (int i = 0; i < ans.size(); i++)
-		std::cout << ans.at(i) << ' ';
+	std::vector < int > nums = { 3,1,3,4,2 };
+	int ans = solutions.findDuplicate(nums);
+	
+
+	std::cout << ans << std::endl;
 
 	return 0;
 }
